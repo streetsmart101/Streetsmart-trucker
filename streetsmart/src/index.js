a322d791db5bdcdbedf8a4152994dc7f1d1cb585
streetsmart101 <prosperwaytrucking@gmail.com>
2026-04-17 11:49:27 -0400

    Update index.js

--- streetsmart/src/index.js
+++ streetsmart/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './StreetSmartTrucker (3)';
+import App from './StreetSmartTrucker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
